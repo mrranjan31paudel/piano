6bd37b9e38d9f6ee855343cfa0832c481de6988f
Ranjan Paudel <paudeljimrranjan@gmail.com>
2020-07-11 10:21:24 +0545

    Updates

---
+++ precache-manifest.47f3136a55d79bf8d94740a505ff2d15.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98fb2e29fee78a071ab93732b64d2200",
+    "url": "/piano/index.html"
+  },
+  {
+    "revision": "7fb1460403e52d7502de",
+    "url": "/piano/static/css/main.603a18b6.chunk.css"
+  },
+  {
+    "revision": "84d0551206f4f4c85dc2",
+    "url": "/piano/static/js/2.54479b38.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/piano/static/js/2.54479b38.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7fb1460403e52d7502de",
+    "url": "/piano/static/js/main.77d0057a.chunk.js"
+  },
+  {
+    "revision": "3bf3f42ad144e33d4b91",
+    "url": "/piano/static/js/runtime-main.887358fa.js"
+  }
+]);
