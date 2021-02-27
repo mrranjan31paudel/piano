196a84470b1671c3ddd44e700fb746115ee72a6e
Ranjan Paudel <paudeljimrranjan@gmail.com>
2021-02-27 17:34:49 +0545

    DEV: Add constants and corresp. files for allowed-keys and key-lists

---
+++ src/constants/allowedKeys.js
@@ -0,0 +1,17 @@
+const ALLOWED_KEYS = {
+  a: true,
+  s: true,
+  d: true,
+  f: true,
+  g: true,
+  h: true,
+  j: true,
+  k: true,
+  w: true,
+  e: true,
+  t: true,
+  y: true,
+  u: true
+};
+
+export default ALLOWED_KEYS;
